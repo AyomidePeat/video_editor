90d7bba21e1e707162709c2d3ccb0fe4a8c5e64d
ayomidepeat <ayomidepeat@gmail.com>
2024-11-22 14:48:26 +0100

    chore: add videoPlayerOption as constructor for videoeditorcontroller

--- lib/src/controller.dart
+++ lib/src/controller.dart
@@ -39,7 +39,7 @@ class VideoEditorController extends ChangeNotifier {
 
   /// Video from [File].
   final File file;
-
+  final VideoPlayerOptions? videoPlayerOptions;
   /// Constructs a [VideoEditorController] that edits a video from a file.
   ///
   /// The [file] argument must not be null.
@@ -51,13 +51,14 @@ class VideoEditorController extends ChangeNotifier {
     this.trimThumbnailsQuality = 10,
     this.coverStyle = const CoverSelectionStyle(),
     this.cropStyle = const CropGridStyle(),
+    this.videoPlayerOptions,
     TrimSliderStyle? trimStyle,
   })  : _video = VideoPlayerController.file(
             File(
               // https://github.com/flutter/flutter/issues/40429#issuecomment-549746165
               Platform.isIOS ? Uri.encodeFull(file.path) : file.path,
             ),
-            videoPlayerOptions: VideoPlayerOptions(mixWithOthers: true)),
+            videoPlayerOptions:videoPlayerOptions ),
         trimStyle = trimStyle ?? TrimSliderStyle(),
         assert(maxDuration > minDuration,
             'The maximum duration must be bigger than the minimum duration');
